3df5734bab02dd3291c4c4ac8b8e7a01ff7c9d4f
Raschaquelle <rachellewalter94@gmail.com>
2021-06-11 11:49:03 -0600

    Basic updates to pages. Fix script for multiple slideshows

--- src/scripts/Script.js
+++ src/scripts/Script.js
@@ -1,23 +1,27 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+
+classNames = ["hikeUpSlides", "ceremonySlides", "hikingDownSlides", "receptionSlides", "honeymoonSlides"]
+for (i = 0; i < classNames.length; i++) {
+    showSlides(slideIndex, classNames[i]);
+}
 
 // Next/previous controls
-function plusSlides(n) {
-showSlides(slideIndex += n);
+function plusSlides(n, className) {
+    showSlides(slideIndex += n, className);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-showSlides(slideIndex = n);
+function currentSlide(n, className) {
+    showSlides(slideIndex = n, className);
 }
 
-function showSlides(n) {
-var i;
-var slides = document.getElementsByClassName("mySlides");
-if (n > slides.length) {slideIndex = 1}
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[slideIndex-1].style.display = "block";
+function showSlides(n, className) {
+    var i;
+    var slides = document.getElementsByClassName(className);
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
 }
